d7ca6ad10c35d7912aba62c380d9ffcf4f0cb651
b4enq <wichitpon045@gmail.com>
2023-04-23 15:05:55 +0700

    firstCommit

---
+++ Maui03/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui03;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Maui03/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui03;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
